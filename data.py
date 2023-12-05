6369a67e04dc4852bee505a26a7aad8c4a76bbe5
thisispaniz <paniz.ziaie@gmail.com>
2023-12-05 17:40:50 +0100

    2

--- data.py
+++ data.py
@@ -1,19 +1,15 @@
-import requests
+import http.client
 
-url = "https://api.ambeedata.com/latest/by-lat-lng?lat={lat}&lng={lng}"
-api_key = "8493379349b84805fdb89ca94c234b1e2245a40db2d80eac801992e4e881a77f"
+conn = http.client.HTTPSConnection("air-quality.p.rapidapi.com")
 
 headers = {
-    "Content-Type": "application/json",
-    "x-api-key": api_key,
+    'X-RapidAPI-Key': "3425d39d3bmshe80957c784ebb11p1552a4jsnaecd6c95eff4",
+    'X-RapidAPI-Host': "air-quality.p.rapidapi.com"
 }
 
-response = requests.get(url, headers=headers)
+conn.request("GET", "/history/airquality?lon=48.1351&lat=11.582", headers=headers)
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    data = response.json()
-    print("Data received:")
-    print(data)
-else:
-    print(f"Error: {response.status_code}, {response.text}")
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
