0376d5a9b7a0897f57db43466f4435ecbc412705
thisispaniz <paniz.ziaie@gmail.com>
2024-01-01 17:40:37 +0100

    ...

--- weather.js
+++ weather.js
@@ -1,10 +1,25 @@
-// Array of locations (latitude and longitude pairs)
+
 const locations = [
     { latitude: 52.52, longitude: 13.41 },
-    // Add other locations here
+    { latitude: 52.52, longitude: 13.40}, //Berlin
+    { latitude: 53.55, longitude: 9.99}, //Hamburg
+    { latitude: 48.14, longitude: 11.58}, //Munich
+    { latitude: 51.23, longitude: 6.78}, //Düsseldorf
+    { latitude: 50.07, longitude: 8.23}, //Wiesbaden
+    { latitude: 48.77, longitude: 9.18}, //Stuttgart
+    { latitude: 53.08, longitude: 8.80}, //Bremen
+    { latitude: 51.05, longitude: 13.74}, //Dresden
+    { latitude: 52.37, longitude: 9.73}, //Hannover
+    { latitude: 50.98, longitude: 11.03}, //Erfurt
+    { latitude: 54.32, longitude: 10.12}, //Kiel
+    { latitude: 52.12, longitude: 11.63}, //Magdeburg
+    { latitude: 49.99, longitude: 8.25}, //Mainz
+    { latitude: 52.39, longitude: 13.06}, //Potsdam
+    { latitude: 49.24, longitude: 6.99}, //Saarbrucken
+    { latitude: 53.64, longitude: 11.40} //Schwerin
   ];
   
-  // Function to fetch data for a location
+
   function fetchDataForLocation(location) {
     const apiUrl = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${location.latitude}&longitude=${location.longitude}&current=european_aqi&forecast_days=1`;
   
@@ -16,17 +31,16 @@ const locations = [
       });
   }
   
-  // Function to create choropleth map for a location
+
   function createChoroplethMapForLocation(data, location) {
-    // Process the data and create the choropleth map here
+
     console.log(`Creating choropleth map for location (${location.latitude}, ${location.longitude}):`, data);
-    // Implement your choropleth map creation logic here
+
   }
-  
-  // Iterate through each location and fetch data
+
   Promise.all(locations.map(location => fetchDataForLocation(location)))
     .then(dataArray => {
-      // dataArray contains the data for each location
+
       dataArray.forEach((data, index) => {
         if (data) {
           createChoroplethMapForLocation(data, locations[index]);
@@ -34,3 +48,5 @@ const locations = [
       });
     });
   
+
+    console.log('Fetching data for location:', location);
