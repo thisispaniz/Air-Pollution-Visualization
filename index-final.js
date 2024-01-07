2e6a634dd3f6edd2c462c50d317065058a4640ac
thisispaniz <paniz.ziaie@gmail.com>
2024-01-07 12:06:16 +0100

    cleaning up

--- index-final.js
+++ index-final.js
@@ -14,7 +14,7 @@ const path = d3.geoPath().projection(projection);
 
 // Load GeoJSON data
 d3.json('2_hoch.geo.json').then(geojson => {
-    const apiUrl = 'https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52,53.55,48.14,51.23,50.07,48.77,53.08,51.05,52.37,50.98,54.32,52.12,49.99,52.39,49.24,53.64&longitude=13.41,9.99,11.58,6.78,8.23,9.18,8.8,13.74,9.73,11.03,10.12,11.63,8.25,13.06,6.99,11.4&current=european_aqi&forecast_days=1&domains=cams_europe';
+    const apiUrl = 'https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52,53.55,48.14,51.23,50.07,48.77,53.08,51.05,52.37,50.98,54.32,52.12,49.99,52.39,49.24,53.64&longitude=13.41,9.99,11.58,6.78,8.23,9.18,8.8,13.74,9.73,11.03,10.12,11.63,8.25,13.06,6.99,11.4&current=european_aqi,pm10,pm2_5,ozone&forecast_days=1&domains=cams_europe';
 
     fetch(apiUrl)
         .then(response => response.json())
@@ -28,7 +28,7 @@ d3.json('2_hoch.geo.json').then(geojson => {
     function renderMap(geoJson, airQualityData) {
         const colorScale = d3.scaleLinear()
             .domain([0, 50, 100]) // Map the domain of your data values
-            .range(['lightblue', 'yellow', 'red']); 
+            .range(['lightgreen', 'yellow', 'brown']); 
 
             const getColor = d => {
                 // Check if the data value is present and valid
@@ -40,7 +40,7 @@ d3.json('2_hoch.geo.json').then(geojson => {
                     console.log('Invalid data for feature:', d);
             
                     // Return a default color for features with missing or invalid data
-                    return 'lightgray';
+                    return 'blue';
                 }
             };
             
