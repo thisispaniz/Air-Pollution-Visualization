17abe0aa4b279e822b67bb88af361479a9595fb2
thisispaniz <paniz.ziaie@gmail.com>
2024-01-08 13:21:12 +0100

    fastapi is up and running

--- static/index-final.js
+++ static/index-final.js
@@ -28,7 +28,7 @@ const colorScale = d3.scaleLinear()
     .range(['#00FFF0', '#3AFF42', '#DBFF00', '#FF8A1F', '#FB504E', '#800000', '#1D0000']);
 
 // Load GeoJSON data
-d3.json('2_hoch.geo.json').then(geojson => {
+d3.json('static/2_hoch.geo.json').then(geojson => {
     const apiUrl = 'https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52,53.55,48.14,51.23,50.07,48.77,53.08,51.05,52.37,50.98,54.32,52.12,49.99,52.39,49.24,53.64&longitude=13.41,9.99,11.58,6.78,8.23,9.18,8.8,13.74,9.73,11.03,10.12,11.63,8.25,13.06,6.99,11.4&current=european_aqi,pm10,pm2_5,ozone&forecast_days=1&domains=cams_europe';
 
     fetch(apiUrl)
