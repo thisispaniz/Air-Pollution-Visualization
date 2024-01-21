5937073187ede5d2c33ec048ed217a395aa3c459
Paniz <99075803+thisispaniz@users.noreply.github.com>
2024-01-21 15:07:19 +0000

    I think it works

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-app.mount("/GEO", StaticFiles(directory="app/GEO"), name="GEO")
-templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/GEO", StaticFiles(directory="GEO"), name="GEO")
+templates = Jinja2Templates(directory="templates")
 
 def makehtml():
     with open("app/templates/index.html", "r", encoding="utf-8") as file:
