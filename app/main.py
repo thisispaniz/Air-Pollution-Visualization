e06b7bf7b9c899dcfa42b6c2e7604f82bcd1fcc8
Paniz <99075803+thisispaniz@users.noreply.github.com>
2024-01-21 13:36:25 +0000

    it finAlly works

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from os import listdir, rename
-from os.path import isfile, join
-import numpy as np
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/GEO", StaticFiles(directory="static"), name="GEO")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/GEO", StaticFiles(directory="app/GEO"), name="GEO")
+templates = Jinja2Templates(directory="app/templates")
 
 def makehtml():
-    with open("templates/index.html", "r", encoding="utf-8") as file:
+    with open("app/templates/index.html", "r", encoding="utf-8") as file:
         return file.read()
 
 
