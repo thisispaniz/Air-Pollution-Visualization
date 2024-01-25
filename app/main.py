bb47a4990c8d74d40ea5a6db2db7100516cf671f
thisispaniz <paniz.ziaie@gmail.com>
2024-01-25 17:50:28 +0100

    upload a json file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -12,8 +13,26 @@ def makehtml():
     with open("templates/index.html", "r", encoding="utf-8") as file:
         return file.read()
 
+def is_json_file(filename):
+    return filename.endswith('.json')
 
 @app.get("/")
 def viewpage():
     html_content = makehtml()
     return HTMLResponse(content=html_content, status_code=200)
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    if not is_json_file(file.filename):
+        raise HTTPException(status_code=400, detail="Uploaded file must be a JSON file")
+    upload_dir = "uploads"
+    os.makedirs(upload_dir, exist_ok=True)
+    file_path = os.path.join(upload_dir, file.filename)
+    
+    if os.path.exists(file_path):
+        raise HTTPException(status_code=400, detail="File with the same name already exists")
+    
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
+        
+    return {"message": "File uploaded successfully", "filename": file.filename}
